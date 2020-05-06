b9416be86851c409bd6b6650c2ba1c8677064a6d
Demoferm <60359246+Demoferm@users.noreply.github.com>
2020-05-05 20:46:16 -0400

    check field

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -5,10 +5,40 @@ import {connect} from 'react-redux';
 function Header (props) {
     console.log(props.state)
     const {field} = props ;
+
     const click = (i) => {
         props.click(i) ;
     }
 
+    const enterName = (i) => {
+        const newName = document.getElementById(`NameOfPlayer${i}`).value;
+        props.nameOfPlayer([i,newName]);
+    }
+    if (props.players[0]===null)
+        return (
+            <div>
+                Enter name of Player1:
+                <p><input type='text' id='NameOfPlayer0'/><button onClick = {() => enterName(0)}>OK</button></p>
+
+            </div>
+
+        )
+    if (props.players[1]===null)
+        return (
+            <div>
+                Enter name of Player2:
+                <p><input type='text' id='NameOfPlayer1'/><button onClick = {() => enterName(1)}>OK</button></p>
+
+            </div>
+
+        )
+    if (props.winner!=null)
+        return (
+            <div>
+                <h1>Winner is {props.players[props.winner]}</h1>
+            </div>
+        )
+    else
     return (
         <div>
             <div className='line'>
@@ -38,6 +68,9 @@ function Header (props) {
             {/*</table>*/}
 
             Current Player: {props.players[props.currentPlayer]}
+            <div>
+                Winner: {props.players[props.winner]}
+            </div>
             {/*<hr/>*/}
             {/*Count {props.count}*/}
             {/*<hr/>*/}
@@ -54,11 +87,13 @@ const mapStateToProps = state => ({
     players: state.players,
     a: state.age,
     count: state.count,
-    currentPlayer: state.currentPlayer
+    currentPlayer: state.currentPlayer,
+    winner: state.winner
 });
 
 const mapDispatchToProps = dispatch => ({
-    click: (i) => dispatch({type: 'CLICK', payload: i})
+    click: (i) => dispatch({type: 'CLICK', payload: i}),
+    nameOfPlayer: (i) => dispatch({type: 'NewName', payload: i})
 });
 
 //const mapDispatchToProps = dispatch => ({});
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,11 +1,12 @@
 const initialState = {
-    players: ['Jim',"Bim"],
+    players: [null, null],
     field: [
         null, null, null,
         null, null, null,
         null, null, null
     ],
-    currentPlayer: 1,
+    currentPlayer: 0,
+    winner: null,
     name: 'Jim',
     count: 7,
     age: 123,
@@ -26,35 +27,67 @@ const reducer = (state = initialState, action) => {
 //         count: state.count - 1
 //     }
 // }
+    const checkWinner = (field) => {
+        if ((field[0] === field[1] && field[0] === field[2] && field[0] != null) ||
+            (field[3] === field[4] && field[3] === field[5] && field[3] != null) ||
+            (field[6] === field[7] && field[6] === field[8] && field[6] != null) ||
+            (field[0] === field[4] && field[0] === field[8] && field[0] != null) ||
+            (field[2] === field[4] && field[2] === field[6] && field[2] != null) ||
+            (field[0] === field[3] && field[0] === field[6] && field[0] != null) ||
+            (field[1] === field[4] && field[1] === field[7] && field[1] != null) ||
+            (field[2] === field[5] && field[2] === field[8] && field[2] != null))
+            return true;
+        else return false;
+    }
 
-switch (action.type)
-{
-    case 'CLICK':
+    switch (action.type) {
+        case 'CLICK':
 
-        const newField = [...state.field];
-        let newCurrentPlayer = state.currentPlayer;
-        if (!newField[action.payload]) {
+            const newField = [...state.field];
+            let winner;
+            let newCurrentPlayer = state.currentPlayer;
+            if (!newField[action.payload]) {
 
 
-            if (state.currentPlayer == 1) {
-                newField[action.payload] = 'O';
-                newCurrentPlayer = 0;
-            } else {
-                newField[action.payload] = 'X';
-                newCurrentPlayer = 1;
+                if (state.currentPlayer == 1) {
+                    newField[action.payload] = 'O';
+                    winner = 1;
+                    newCurrentPlayer = 0;
+                } else {
+                    newField[action.payload] = 'X';
+                    winner = 0;
+                    newCurrentPlayer = 1;
+                }
+
             }
-        }
 
-        return {
-            ...state,
-            field: newField,
-            currentPlayer: newCurrentPlayer
-        };
+            if (checkWinner(newField))
+                return {
+                    ...state,
+                    field: newField,
+                    currentPlayer: newCurrentPlayer,
+                    winner: winner
+                };
+            else return {
+                ...state,
+                field: newField,
+                currentPlayer: newCurrentPlayer,
+            };
+
+        case 'NewName':
+            const newPlayers = [...state.players];
+            console.log(newPlayers);
+            newPlayers[action.payload[0]] = action.payload[1];
+
+            return {
+                ...state,
+                players: newPlayers
+            };
 
 
-    default:
-        return state;
-}
+        default:
+            return state;
+    }
     // return {
     //     name: "John",
     //     age: 123,
